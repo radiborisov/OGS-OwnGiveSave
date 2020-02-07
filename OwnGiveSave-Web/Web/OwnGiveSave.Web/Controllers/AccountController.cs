78990416fc2b0eeedadbe397e7caa8c93c897269
IliqVulov <iliqvulov@abv.bg>
2020-02-07 13:34:15 +0200

    Commit

--- OwnGiveSave-Web/Web/OwnGiveSave.Web/Controllers/AccountController.cs
+++ OwnGiveSave-Web/Web/OwnGiveSave.Web/Controllers/AccountController.cs
@@ -1,12 +1,7 @@
-﻿
-namespace OwnGiveSave.Web.Controllers
+﻿namespace OwnGiveSave.Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using OwnGiveSave.Data.Models;
