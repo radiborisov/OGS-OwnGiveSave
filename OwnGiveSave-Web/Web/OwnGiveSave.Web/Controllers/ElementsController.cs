5c46329f05f263d872910caef8372fd187992337
IliqVulov <iliqvulov@abv.bg>
2020-02-19 18:29:56 +0200

    Added template for the views

---
+++ OwnGiveSave-Web/Web/OwnGiveSave.Web/Controllers/ElementsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OwnGiveSave.Web.Controllers
+{
+    public class ElementsController : BaseController
+    {
+        // GET: Elements
+        public IActionResult Elements()
+        {
+            return this.View();
+        }
+
+    }
+}
